efdf60178ba5a8ac3a047a236bf40e7652393ddc
renatogual <renatogual@hotmail.com>
2021-07-13 22:43:59 -0300

    Fazendo implementação do modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
-import { Header } from './components/Header';
-import { Dashboard } from './components/Dashboard';
-import { GlobalStyle } from './styles/GlobalStyle';
+import { useState } from "react";
+import { createServer } from "miragejs";
+import Modal from "react-modal";
+
+import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+import { GlobalStyle } from "./styles/GlobalStyle";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [];
+    });
+  },
+});
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isOpenNewTransactionModal, setIsOpenNewTransactionModal] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsOpenNewTransactionModal(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsOpenNewTransactionModal(false);
+  }
+
   return (
-  <>
-    <Header />
-    <Dashboard />
-    <GlobalStyle />
-  </>);
+    <>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal
+        isOpen={isOpenNewTransactionModal}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <GlobalStyle />
+    </>
+  );
 }
